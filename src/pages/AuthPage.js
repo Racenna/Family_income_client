d2f9c754d8d2ba52b6e549e841ce539ae42789e1
Racenna <mrvader228@gmail.com>
2020-02-07 13:41:33 +0300

    Hooks and registration

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHttp } from "../hooks/http.hook";
+import { useMessage } from "../hooks/message.hook";
 
 export const AuthPage = () => {
+  const message = useMessage();
+  const { loading, error, request, clearError } = useHttp();
+  const [form, setForm] = useState({
+    email: "",
+    pwd: ""
+  });
+
+  useEffect(() => {
+    message(error);
+    clearError();
+  }, [error, message, clearError]);
+
+  const changeHandler = event => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+  const registerHandler = async () => {
+    try {
+      const data = await request("/api/auth/register", "POST", { ...form });
+      message(data.message);
+    } catch (error) {}
+  };
+
   return (
     <div className="row">
       <div className="col s6 offset-s3">
@@ -14,7 +39,9 @@ export const AuthPage = () => {
                   placeholder="Enter email"
                   id="email"
                   type="email"
+                  name="email"
                   className="validate"
+                  onChange={changeHandler}
                 />
                 <label htmlFor="email">Email</label>
               </div>
@@ -23,16 +50,25 @@ export const AuthPage = () => {
                   placeholder="Enter password"
                   id="password"
                   type="password"
-                  name="password"
+                  name="pwd"
                   className="validate"
+                  onChange={changeHandler}
                 />
                 <label htmlFor="password">Password</label>
               </div>
             </div>
           </div>
           <div className="card-action">
-            <button className="btn">Login</button>
-            <button className="btn">Registrate</button>
+            <button className="btn" disabled={loading}>
+              Login
+            </button>
+            <button
+              className="btn"
+              onClick={registerHandler}
+              disabled={loading}
+            >
+              Registrate
+            </button>
           </div>
         </div>
       </div>
